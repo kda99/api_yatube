886dd8abe3cdc05a23e302441ecf1eadea3ab0d6
Дмитрий Капустин <kda99@mail.ru>
2023-04-10 19:47:53 +0300

    1

--- yatube_api/posts/views.py
+++ yatube_api/posts/views.py
@@ -2,6 +2,7 @@
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
+from rest_framework.response import Response
 
 from .models import Comment, Post, User, Group
 
@@ -79,8 +80,22 @@ class CommentViewSet(viewsets.ModelViewSet):
         return post.comments.all()
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     """Gsadada."""
 
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+    def perform_create(self, serializer):
+        """Create a new group."""
+        if serializer.instance.author == 'admin':
+            post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+            serializer.save(author=self.request.user, post = post)
+        return Response(status=405)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author == 'admin':
+            if serializer.instance.author != self.request.user:
+                raise PermissionDenied('Изменение чужого контента запрещено!')
+            super(GroupViewSet, self).perform_update(serializer)
+        return Response(status=405)
